b7e89f476f93172475dabf207b6e00ab8ec48f09
Anjum Ahuja <jack8daniels2@gmail.com>
2014-11-24 13:58:36 -0800

    Delete ipv4_serde.py

--- utils/utils/ipv4_serde.py
+++
@@ -1,8 +0,0 @@
-import socket
-import struct
-
-def encode(ip):
-    return struct.unpack("!I", socket.inet_aton(ip))[0]
-
-def decode(encoded):
-    return socket.inet_ntoa(struct.pack("!I", encoded))
